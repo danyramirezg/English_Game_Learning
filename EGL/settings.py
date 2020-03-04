110e45bc0bef2e94e130ca0e407b9c72c5142d0f
joshuaciencia <joshuaciencia@gmail.com>
2020-03-04 15:27:17 -0500

    switched from sqllie to mysql server

--- EGL/settings.py
+++ EGL/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'EGL.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'EGL',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
