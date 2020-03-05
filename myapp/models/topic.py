e6af4be7bd8245a9296c9780ecf8e0f8815d8f73
Dany <danyramirezg@gmail.com>
2020-03-05 11:46:06 -0500

    Add -str- method

--- myapp/models/topic.py
+++ myapp/models/topic.py
@@ -5,7 +5,10 @@ from django.contrib.auth.models import User
 
 
 class Topic(models.Model):
-    name = models.CharField(max_length=250)
+    name = models.CharField(max_length=250, unique=True)
     detail = models.CharField(max_length=2048)
     createdAt = models.DateTimeField(auto_now_add=True)
     updatedAct = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return self.name
