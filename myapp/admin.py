0b71441902ba1bd6383f8e16ff81606a731f24e4
yenntobon <ymtobony@unal.edu.co>
2020-03-05 12:57:32 -0500

    add word

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from .models.topic import Topic
 from .models.user import User
+from .models.word import Word
 # Register your models here.
-
+ 
 admin.site.register(Topic)
 admin.site.register(User)
+admin.site.register(Word)
