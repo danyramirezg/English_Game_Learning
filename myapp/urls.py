cef03eda19250622ca9a4cc34dd617930276a50a
yenntobon <ymtobony@unal.edu.co>
2020-03-06 15:35:36 -0500

    add method POST, DELETE PUT

--- myapp/urls.py
+++ myapp/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
             path('topic/<int:pk>/', topic.topic_detail),
             #word endpoints
             path('api/pod_words/', word.word_list),
-            #path('api/pod_Words/<int:pk>/', word.word_detail),
+            path('api/pod_words/<int:pk>/', word.word_detail),
             # translator API endpoint
             path('api/pod_words/<word>', translator.translate),
         ]
