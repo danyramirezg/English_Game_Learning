1e0438d7f8c7339a6cff150e4b06c27658c4d921
Dany <danyramirezg@gmail.com>
2020-03-06 23:27:14 -0500

    Add endpoint in topic

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,15 +3,15 @@ from myapp.views import user, word, topic
 from myapp.views import translator
 
 urlpatterns = [
-            # User endpoints
-            path('api/pod_users/', user.user_list),
-            path('api/pod_users/<int:pk>/', user.user_detail),
-            # Topic endpoints
-            path('topic/', topic.topic_list),
-            path('topic/<int:pk>/', topic.topic_detail),
-            #word endpoints
-            path('api/pod_words/', word.word_list),
-            #path('api/pod_Words/<int:pk>/', word.word_detail),
-            # translator API endpoint
-            path('api/pod_words/<word>', translator.translate),
-        ]
+    # User endpoints
+    path('api/pod_users/', user.user_list),
+    path('api/pod_users/<int:pk>/', user.user_detail),
+    # Topic endpoints
+    path('api/pod_topic/', topic.topic_list),
+    path('api/pod_topic/<int:pk>/', topic.topic_detail),
+    # word endpoints
+    path('api/pod_words/', word.word_list),
+    # path('api/pod_Words/<int:pk>/', word.word_detail),
+    # translator API endpoint
+    path('api/pod_words/<word>', translator.translate),
+]
