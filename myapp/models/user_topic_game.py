2a4fb2f2c0bea7a59fe14b9aad51ea75dcead9ed
yenntobon <ymtobony@unal.edu.co>
2020-03-07 11:08:50 -0500

    add all models correction

--- myapp/models/user_topic_game.py
+++ myapp/models/user_topic_game.py
@@ -2,9 +2,13 @@ from django.db import models
 
 """" This class to relationship between user and topic, which user create which topics"""
 
-class User_Topic(models.Model):
+class User_Topic_Game(models.Model):
     user = models.ForeignKey("User", on_delete=models.CASCADE)
     topic = models.ForeignKey("Topic", on_delete=models.CASCADE)
+    #game = models.ForeignKey("Topic", on_delete=models.CASCADE)
+    option = models.ForeignKey("Option_Game", on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.user + self.topic 
+        return self.user + self.topic
+
+        # + self.game
--- myapp/models/word.py
+++ myapp/models/word.py
@@ -6,7 +6,7 @@ class Word(models.Model):
    user = models.ForeignKey("User", on_delete=models.CASCADE)
    topic = models.ForeignKey("Topic", on_delete=models.CASCADE)
    word = models.CharField(max_length=30, blank=False, null=False)
-   traslation = models.CharField(max_length=30, blank=False, null=False)
+   translation = models.CharField(max_length=30, blank=False, null=False)
    access = models.BooleanField(default=True, editable=False)
    attemps = models.IntegerField(default=0, editable=False)
    created_at = models.DateTimeField(auto_now_add=True, editable=False)
