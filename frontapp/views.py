5aeb7f5b0bec457afd454a1e5958993357a7bb4a
Joshuaciencia <joshuaciencia2016@gmail.com>
2020-03-07 21:38:15 -0500

    added register template and user form creation

--- frontapp/views.py
+++ frontapp/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserForm
 
 # Create your views here.
 
@@ -11,5 +12,22 @@ def login(request):
     return render(request, 'frontapp/login.html')
 
 
+def register(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = UserForm()
+
+    return render(request, 'frontapp/register.html', {'form': form})
+
+
 def snake(request):
     return render(request, 'frontapp/snake.html')
+
+
+def root(request):
+    return redirect('home')
