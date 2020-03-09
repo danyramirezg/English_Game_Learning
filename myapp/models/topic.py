bdbfaec675856808b74a905c8530e3f1b1fd8798
Dany <danyramirezg@gmail.com>
2020-03-08 22:03:36 -0500

    Change code from camelcase to underscore

--- myapp/models/topic.py
+++ myapp/models/topic.py
@@ -1,17 +1,16 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 """" This class create a Topic model """
 
 
 class Topic(models.Model):
-    name = models.CharField(max_length=250, unique=True)
-    detail = models.CharField(max_length=2048)
-    createdAt = models.DateTimeField(auto_now_add=True)
-    updatedAct = models.DateTimeField(null=True)
+    topic_name = models.CharField(max_length=250, blank=False, null=False)
+    detail = models.CharField(max_length=2048, blank=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now_add=True, editable=False)
 
     def __str__(self):
-        return self.name
+        return self.topic_name
 
     class Meta:
-        ordering = ['createdAt']
+        ordering = ['created_at']
