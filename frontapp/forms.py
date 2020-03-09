fecf847e7fe7ba8377cbabd33d00cc448a636e71
Joshuaciencia <joshuaciencia2016@gmail.com>
2020-03-08 20:46:36 -0500

    added login form, authentication and passowrd confirm field

--- frontapp/forms.py
+++ frontapp/forms.py
@@ -1,11 +1,43 @@
 from django.forms import ModelForm
 from django import forms
 from myapp.models.user import User
+from django.contrib.auth import authenticate
 
 
 class UserForm(ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
+
 
     class Meta:
         model = User
-        fields = ['user_name', 'password', 'cell_phone', 'email', 'word_attemp']
+        fields = ['user_name', 'cell_phone', 'email', 'word_attemp', 'password']
+
+    def clean(self):
+        cleaned_data = super(UserForm, self).clean()
+        
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+
+        # test if passwords match
+
+        if password != confirm_password:
+            raise forms.ValidationError(
+                    "passwords do not match"
+                    )
+
+        # test if user already exists
+        user_name = cleaned_data.get("user_name")
+        if User.objects.filter(user_name=user_name).exists():
+            raise forms.ValidationError("User name {} already exists".format(user_name))
+
+class LoginForm(forms.Form):
+    user_name = forms.CharField(label="User name", max_length=60)
+    password = forms.CharField(label="Password", widget=forms.PasswordInput, max_length=50)
+
+    def clean(self):
+        user_name = self.cleaned_data.get('user_name')
+        password = self.cleaned_data.get('password')
+        user = User.objects.filter(user_name=user_name)
+        if not user.exists() or user.get(user_name=user_name).password != password:
+            raise forms.ValidationError("username or password wrong")
