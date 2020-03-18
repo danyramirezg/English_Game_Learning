9fc7924957cf564f4d35c2753ce021fa4d6923f7
Dany <danyramirezg@gmail.com>
2020-03-18 00:48:59 -0500

    Add database information

--- backup.sql
+++ backup.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.7.29, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.28, for Linux (x86_64)
 --
 -- Host: localhost    Database: EGL
 -- ------------------------------------------------------
--- Server version	5.7.29-0ubuntu0.18.04.1
+-- Server version	5.7.28-0ubuntu0.18.04.4
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -98,7 +98,7 @@ CREATE TABLE `myapp_topic` (
   `updated_at` datetime(6) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `myapp_topic_topic_name_a5868a05_uniq` (`topic_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -107,7 +107,7 @@ CREATE TABLE `myapp_topic` (
 
 LOCK TABLES `myapp_topic` WRITE;
 /*!40000 ALTER TABLE `myapp_topic` DISABLE KEYS */;
-INSERT INTO `myapp_topic` VALUES (4,'House','Parts of the house','2020-03-14 05:17:23.806920','2020-03-14 05:17:23.803155');
+INSERT INTO `myapp_topic` VALUES (4,'House','Parts of the house','2020-03-14 05:17:23.806920','2020-03-14 05:17:23.803155'),(5,'Professions','List vocabulary different professions','2020-03-17 00:00:00.000000','2020-03-17 00:00:00.000000');
 /*!40000 ALTER TABLE `myapp_topic` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -120,4 +120,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-03-14  0:26:31
+-- Dump completed on 2020-03-18  0:47:09
